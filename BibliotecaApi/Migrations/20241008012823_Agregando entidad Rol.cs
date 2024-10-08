0e7cbb7b5486c0a121df8743d9253cbfbed53ba8
Mariano Frias <frias.mariano@gmail.com>
2024-10-07 21:33:55 -0400

    Migración para agregar Entidad 'Rol'

---
+++ BibliotecaApi/Migrations/20241008012823_Agregando entidad Rol.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BibliotecaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregandoentidadRol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "RolId",
+                table: "Usuarios",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.CreateTable(
+                name: "Rol",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rol", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_RolId",
+                table: "Usuarios",
+                column: "RolId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Usuarios_Rol_RolId",
+                table: "Usuarios",
+                column: "RolId",
+                principalTable: "Rol",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Usuarios_Rol_RolId",
+                table: "Usuarios");
+
+            migrationBuilder.DropTable(
+                name: "Rol");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Usuarios_RolId",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "RolId",
+                table: "Usuarios");
+        }
+    }
+}
--- BibliotecaApi/Models/Usuario.cs
+++ BibliotecaApi/Models/Usuario.cs
@@ -7,8 +7,10 @@ namespace BibliotecaApi.Models
         public long Id { get; set; }
         public string Email { get; set; } = null!;
         public long PersonaId { get; set; }
+        public long RolId { get; set; }
 
         public required Persona Persona { get; set; }
+        public required Rol Rol { get; set; }
         public ICollection<UsuarioToken>? Tokens { get; set; }
     }
 }
